f7a412088aec9e5ded333f70a86e7b362f12b23a
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-10-07 12:32:29 +1100

    connected express app with local db. Imported comments and users data to database. Connection test - working. DB test using mongo shell - working

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(express.json());
 app.listen(3000, console.log('Listening on Port 3000'));
+
+
+mongoose.connect('mongodb://localhost:27017/sample_mflix', {useUnifiedTopology: true, useNewUrlParser: true}, (err, success) => {
+  if (err) { return console.error(err)}
+  console.log('Connection Status: Success');
+})
